38b7c531d7bd77d1f43342fbb84f28cae7ff0b84
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2021-12-29 21:58:14 +0100

    Update page1.bat

--- Pages/page1.bat
+++ Pages/page1.bat
@@ -1,5 +1,162 @@
 @echo off
-set dapps=1
+:start
+title [!] %titlemain% - SEITE 1
+setlocal enabledelayedexpansion
+mode con cols=100 lines=30
+set no=1
+:: properties
+set max=9
+set defcolor=0f
+set sel=1
+set selcolor=8f
+call :reset
+call :setmenu
+:refresh
+:: redraw window
+DCls 0
+call :recolor
+call :title
+call :menu
+call :below
+set sel2=!sel!
+DKey
+if "%errorlevel%"=="13" call :action&goto refresh
+if "%errorlevel%"=="40" set /a sel+=1&goto refresh
+if "%errorlevel%"=="38" set /a sel-=1&goto refresh
+goto refresh
+:reset
+:: reset variable
+for /l %%i in (1 1 !max!) do set color%%i=!defcolor!
+for /l %%i in (1 1 !max!) do set menu%%i=undefined
+exit /b
 
-set 01app=PACKAGE MANAGER
-set 01gnm=PAPER-PCKG
+:menu
+:: display menu
+color !defcolor!
+for /l %%i in (1 1 !max!) do (
+DCls x1
+set /p ="!left!"<nul
+DColor !color%%i!
+set /p ="!menu%%i!"<nul
+DColor !defcolor!
+echo.!right!
+)
+exit /b
+
+:title
+echo.
+echo  %program%
+echo.  
+echo. 
+exit /b
+
+:below
+echo.
+echo.
+echo. BITTE MIT PFEILTASTEN AUSWAEHLEN
+echo. ZUM BESTAETIGEN [ENTER] DRUECKEN
+exit /b
+
+:recolor
+:: reset color
+if !sel! lss 1 set sel=!max!
+if !sel! gtr !max! set sel=1
+set color!sel!=!selcolor!
+if not !sel!==!sel2! (
+set color!sel2!=!defcolor!
+)
+exit /b
+
+
+
+
+:setmenu
+:: set menu caption
+set menu1=PAKET MANGER
+set menu2=LEER        
+set menu3=LEER        
+set menu4=LEER        
+set menu5=LEER         
+set menu6=LEER        
+set menu7=LEER        
+set menu8=ZURUECK     
+set menu9=NAECHSTE    
+set left=
+set right=
+exit /b
+
+
+
+
+:action
+if !sel!==5 (
+if !no!==1 (
+set selcolor=3f
+set defcolor=70
+)
+if !no!==2 (
+set selcolor=0b
+set defcolor=0f
+)
+if !no!==3 (
+set selcolor=8f
+set defcolor=0f
+set no=0
+)
+call :reset
+call :setmenu
+set /a no+=1
+) else (
+cls
+if !sel! EQU 1 (
+
+echo PAPER-PCKG > app-name.tmp
+
+)
+if !sel! EQU 2 (
+
+goto start
+
+)
+if !sel! EQU 3 (
+
+goto start
+
+)
+if !sel! EQU 4 (
+
+goto start
+
+)
+if !sel! EQU 5 (
+
+goto start
+
+)
+if !sel! EQU 6 (
+
+goto start
+
+)
+if !sel! EQU 7 (
+
+goto start
+
+)
+if !sel! EQU 8 (
+
+cls
+start.bat
+
+)
+if !sel! EQU 9 (
+
+if exist C:\PAPERSTUDIOS\Temp\upai-pages\pg2.bat (
+pg2.bat
+) else (
+goto start
+)
+
+)
+)
+exit /b
