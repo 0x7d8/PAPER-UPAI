6cf8fcb2768ec2288a1660d3e21bc0f522c25d03
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2022-01-08 10:06:34 +0100

    Update page1.bat

--- Pages/page1.bat
+++ Pages/page1.bat
@@ -7,9 +7,17 @@ mode con cols=100 lines=30
 set no=1
 :: properties
 set max=9
-set defcolor=0f
 set sel=1
+if %theme% EQU 1 (
+set selcolor=0f
+set defcolor=70
+goto skip2nd
+)
+if %theme% EQU 0 (
 set selcolor=8f
+set defcolor=0f
+)
+:skip2nd
 call :reset
 call :setmenu
 :refresh
