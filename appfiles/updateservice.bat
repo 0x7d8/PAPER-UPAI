784228d6388702f5928b668f24f0f94e9a643ce9
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2022-03-03 21:41:38 +0100

    Add files via upload

--- appfiles/updateservice.bat
+++ appfiles/updateservice.bat
@@ -1,11 +1,19 @@
 @echo off
+cls
+echo.
+echo  [i] DIE UPDATE DATEIEN WERDEN HERUNTERGELADEN,
+echo      BITTE WARTEN SIE EINEN MOMENT.
 call internet.cmd https://github.com/rotvproHD/PAPER-UPAI/raw/main/appfiles/APPFILES.zip -saveTo C:\PAPERSTUDIOS\Temp\appupdate.zip
 for %%i in (*.*) do if not "%%i"=="updateservice.bat" del /q "%%i"
 del /q C:\PAPERSTUDIOS\Data\UPAI-de\versions
 rmdir C:\PAPERSTUDIOS\Data\UPAI-de\versions
 "archiver\WinRAR.exe" x "C:\PAPERSTUDIOS\Temp\appupdate.zip" *.* C:\PAPERSTUDIOS\Data\UPAI-de\
+PING localhost -n 3 >NUL
 cls
 echo.
-echo [!] FERTIG
+echo  [i] DAS UPDATE WURDE INSTALLIERT,
+echo      HERZLICHEN GLUECKWUNSCH.
+echo.
 pause
+cls
 boot.bat
