f373150027e145ff73bf00e928766ad6c7d4cb6d
rotvproHD <63073556+rotvproHD@users.noreply.github.com>
2022-03-04 09:12:56 +0100

    Add files via upload

--- appfiles/updateservice.bat
+++ appfiles/updateservice.bat
@@ -3,27 +3,27 @@ cls
 echo.
 echo  [i] DIE UPDATE DATEIEN WERDEN HERUNTERGELADEN,
 echo      BITTE WARTEN SIE EINEN MOMENT.
-PING localhost -n 3 >NUL
+PING localhost -n 6 >NUL
 call internet.cmd https://github.com/rotvproHD/PAPER-UPAI/raw/main/appfiles/APPFILES.zip -saveTo C:\PAPERSTUDIOS\Temp\appupdate.zip
 for %%i in (*.*) do if not "%%i"=="updateservice.bat" del /q "%%i"
 cls
 echo.
 echo  [i] ES IST AM BESTEN, WENN SIE IMMER DIE
 echo      NEUSTE VERSION UNSERER SOFTWARE BESITZEN
-PING localhost -n 3 >NUL
+PING localhost -n 6 >NUL
 del /q C:\PAPERSTUDIOS\Data\UPAI-de\versions
 rmdir C:\PAPERSTUDIOS\Data\UPAI-de\versions
 cls
 echo.
 echo  [i] FALLS FEHLER IN DER NEUEN VERSION AUFTRETEN,
 echo      KOENNEN SIE DIE IN UPAAI UNTER FORMS MELDEN
-PING localhost -n 3 >NUL
+PING localhost -n 6 >NUL
 "archiver\WinRAR.exe" x "C:\PAPERSTUDIOS\Temp\appupdate.zip" *.* C:\PAPERSTUDIOS\Data\UPAI-de\
 cls
 echo.
 echo  [i] DAS UPDATE IST FAST FERTIG INSTALLIERT,
 echo      NUR NOCH WENIGE MOMENTE
-PING localhost -n 3 >NUL
+PING localhost -n 6 >NUL
 cls
 echo.
 echo  [i] DAS UPDATE WURDE INSTALLIERT,
